7ea52c83bea92cc1e82023332caa3a1793a64a90
Brock McElroy <28877984+brxck@users.noreply.github.com>
2019-11-27 12:42:58 -0700

    Cleanup CRR command sets

--- castervoice/lib/ccr/__init__.py
+++ castervoice/lib/ccr/__init__.py
@@ -3,30 +3,14 @@
 from castervoice.lib import utilities
 
 command_sets = {
-    # "bash.bash": ("Bash", ),
     "core.alphabet": ("Alphabet", ),
     "core.nav": ("Navigation", ),
     "core.numbers": ("Numbers", ),
     "core.punctuation": ("Punctuation", ),
     "core.text_manipulation": ("TextManipulation", ),
-    # "cpp.cpp": ("CPP", ),
-    # "csharp.csharp": ("CSharp", ),
-    # "dart.dart": ("Dart", ),
     "voice_dev_commands.voice_dev_commands": ("VoiceDevCommands", ),
-    # "go.go": ("Go", ),
-    # "haxe.haxe": ("Haxe", ),
-    # "html.html": ("HTML", ),
-    # "java.java": ("Java", ),
     "javascript.javascript": ("Javascript", ),
-    # "latex.latex": ("LaTeX", ),
-    # "markdown.markdown": ("Markdown", ),
-    # "matlab.matlab": ("Matlab", ),
     "python.python": ("Python", ),
-    # "r.r": ("Rlang", ),
-    # "rust.rust": ("Rust", ),
-    # "sql.sql": ("SQL", ),
-    # "prolog.prolog": ("Prolog", ),
-    # "vhdl.vhdl": ("VHDL", ),
 }
 
 for module_name, class_name_tup in command_sets.iteritems():
